8e78788944469892efdb4df76857cde6c98f9b57
Liam Hockley <xdega121@gmail.com>
2019-11-16 14:17:11 -0600

    Remove extra gitconfig copy; Remove bash_profile copy

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 # Copy Dotfiles
-cp  ~/.dotfiles/.bash_profile ~
 cp ~/.dotfiles/.gitconfig ~
 cp ~/.dotfiles/.gitignore_global ~
 cp  ~/.dotfiles/.vimrc ~
 cp -R ~/.dotfiles/.vim ~
-cp ~/.dotfiles/.gitconfig ~
-
